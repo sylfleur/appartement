1df08ff04f978d1b594d3b87d35205d7246e8051
sylfleur <cfleurian@sylfleur.com>
2022-05-02 15:08:50 +0100

    Creation de la fonction CreateProprietaire

--- lib/screens/create.dart
+++ lib/screens/create.dart
@@ -1,6 +1,11 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import '../models/proprietaire.dart';
 
 class Create extends StatefulWidget {
   const Create({Key? key}) : super(key: key);
@@ -10,13 +15,15 @@ class Create extends StatefulWidget {
 }
 
 class _CreateState extends State<Create> {
-
-   final _formKey = GlobalKey<FormState>();
+  final _formKey = GlobalKey<FormState>();
 
   final nomController = TextEditingController();
   final prenomController = TextEditingController();
   final telephoneController = TextEditingController();
   final ifuController = TextEditingController();
+
+  // bool ifu = true;
+  dynamic _ifu = '';
 
   @override
   void initState() {
@@ -27,6 +34,10 @@ class _CreateState extends State<Create> {
 
   @override
   Widget build(BuildContext context) {
+    var nom = nomController.text;
+    var prenom = prenomController.text;
+    var telephone = telephoneController.text;
+    var ifu = _ifu;
     return Scaffold(
       appBar: AppBar(
         title: const Text('CREATE'),
@@ -35,7 +46,8 @@ class _CreateState extends State<Create> {
         padding: const EdgeInsets.all(20.0),
         child: Column(
           children: [
-            Form(key: _formKey,
+            Form(
+              key: _formKey,
               child: Column(
                 children: [
                   TextFormField(
@@ -45,41 +57,85 @@ class _CreateState extends State<Create> {
                     controller: nomController,
                   ),
                   TextFormField(
-                      decoration: InputDecoration(
-                        labelText: 'prenom',
-                      ),
-                      controller: prenomController,),
-                  TextFormField(
                     decoration: InputDecoration(
-                      labelText: 'telephone',
-                    ),controller: telephoneController
+                      labelText: 'prenom',
+                    ),
+                    controller: prenomController,
                   ),
                   TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'Ifu',
-                    ),controller: ifuController,
+                      decoration: InputDecoration(
+                        labelText: 'telephone',
+                      ),
+                      controller: telephoneController),
+                  Text((('Ifu')),
+                      style: TextStyle(
+                        fontSize: 20,
+                      )),
+                  RadioListTile(
+                    title: Text('Oui'),
+                    value: 1,
+                    groupValue: _ifu,
+                    onChanged: (value) {
+                      setState(
+                        () {
+                          _ifu = value;
+                        },
+                      );
+                    },
+                  ),
+                  RadioListTile(
+                    title: Text('Non'),
+                    value: 2,
+                    groupValue: _ifu,
+                    onChanged: (value) {
+                      setState(
+                        () {
+                          _ifu = value;
+                        },
+                      );
+                    },
                   ),
                   ElevatedButton(
                     onPressed: () {
-                      if (_formKey.currentState!.validate()) {
-                      }
-                      setState(() {
-                        
-                      });
+                      if (_formKey.currentState!.validate()) {}
+                      setState(() {});
                       //Navigator.pop(context);
                     },
                     child: Text('Save'),
-                  ),
+                  )
                 ],
               ),
             ),
-            Text(nomController.text),
-            Text(prenomController.text),
-            Text(telephoneController.text),
-            Text(ifuController.text),
+            Text(nom),
+            Text(prenom),
+            Text(telephone),
+            Text(              
+              '${ifu == 1 ? true : false}',
+            ),
           ],
         ),
       ),
     );
+  }
+}
+
+Future<Proprietaire> createProprietaire(
+    String nom, String prenom, String telephone, bool ifu) async {
+  final response = await http.post(
+    Uri.parse('http://directus.sylfleur.com:8055/items/proprietaire/'),
+    headers: <String, String>{
+      'content-type': 'application/json; charset=UTF-8',
+    },
+    body: jsonEncode(<String, dynamic>{
+      "nom": nom,
+      "prenom": prenom,
+      "telephone": telephone,
+      "disposeIfu": ifu,
+    }),
+  ); 
+  if (response.statusCode == 201) {
+    return Proprietaire.fromJson(json.decode(response.body));
+  } else {
+    throw Exception('Failed to load post');
   }
 }
